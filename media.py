6a9629a1ebb7909e486a8c3857bd4301729baf1c
Eduhj <eduardo.jonerhahn@gmail.com>
2026-03-23 21:52:38 -0300

    Expandindo a migracao do terminal para o gui

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
 
-# Bibliotecas
+###BIBLIOTECAS###
 import tkinter as tk
 from tkinter import ttk
 
-# Variáveis
+###VARIÁVEIS###
 notas = []
 
 ###JANELA###
@@ -15,39 +15,49 @@ frame = ttk.Frame(janela, padding="20")
 frame.grid()
 
 # Elementos
-label = ttk.Label(frame, text="Digite as notas e clique em 'Calcular Média'")
+label = ttk.Label(frame, text="Digite e adicione as notas e clique em 'Calcular Média'")
 label.grid(row=0, column=0, pady=(0, 12))
 
+
 entrada = ttk.Entry(frame, width=20)
-entrada.grid(row=1, column=0, pady=(4, 12))
+entrada.grid(row=2, column=0, pady=(4, 12))
 
 botao = ttk.Button(frame, text="Calcular Média", command=lambda: calculing(notas))
-botao.grid(row=2, column=0, pady=(4, 12))
+botao.grid(row=3, column=1, pady=(4, 12))
 
-# Função para calcular a média
+botao_add = ttk.Button(frame, text="Add", command=lambda: add())
+botao_add.grid(row=3, column=0, pady=(4, 12))
+
+label = ttk.Label(frame, text="Digite as notas e clique em 'Calcular Média'")
+label.grid(row=4, column=0, pady=(0, 12))
+
+###FUNÇÕES###
+
+# Calcular a média
 def calculing(notas):
-    resultado = sum(notas) / len(notas)
-    print(f"soma: {sum(notas)}, quantidade: {len(notas)}, média: {resultado}")
-    notas.clear()
-    if resultado >= 7:
-        print("Aprovado")
-    else:
-        print("Reprovado")
-
-# Loop para receber as notas
-while True:
-    try:
-        input_nota = input("Digite uma nota (ou 'calc' para calcular): ")
-        if input_nota == 'calc' and len(notas) > 0:
-            calculing(notas)
-
-        elif float(input_nota) < 0 or float(input_nota) > 10:
-            print("Nota inválida. Por favor, digite um número entre 0 e 10.")
-            continue
+    if len(notas) > 0:
+        resultado = sum(notas) / len(notas)
+        print(f"soma: {sum(notas)}, quantidade: {len(notas)}, média: {resultado}")
+        notas.clear()
+        if resultado >= 7:
+            print("Aprovado")
         else:
-            notas.append(float(input_nota))
+            print("Reprovado")
 
+# Receber as notas
+def add():
+    try:
+        input_nota = float(entrada.get())
+
+        if input_nota < 0 or input_nota > 10:
+            print("Nota inválida. Por favor, digite um número entre 0 e 10.")
+            
+        else:
+            notas.append(input_nota)
+            label_notas= ttk.Label(frame, text=str(notas))
+            label_notas.grid(row=1, column=0, pady=(0, 12))
+            
     except ValueError:
         print("Entrada inválida. Por favor, digite um número ou 'calc'.")
 
-
+janela.mainloop()
